b4679e88b52460b7d3fdd4a04fd3a82d561bf7c7
Alexis Santiago <alexis.santiago@taxact.com>
2023-11-28 17:53:05 -0600

    add flowbite and modal

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  css: [
+    '~/assets/css/input.css',
+  ],
+  extends: [         // Extend from an installed npm package
+    'github:AlexGiovanni/layer1', // Extend from a git repository
+  ]
 })
